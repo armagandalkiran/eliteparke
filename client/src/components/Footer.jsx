6cad5710a2ba2b21df0cda7feec76de940961e83
Armagan Dalkiran <armagandalkran20@gmail.com>
2021-08-12 13:42:42 +0300

    ✔ font-family added

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function Footer() {
     return (
         <footer>
-            
+            <div>
+                asdasdasdsad
+            </div> 
         </footer>
     )
 }

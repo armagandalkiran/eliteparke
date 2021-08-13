8ad1df3d3748b368771fb8b90e1e05a0f7c2e6ef
Armagan Dalkiran <armagandalkran20@gmail.com>
2021-08-13 17:32:49 +0300

    🐱‍👤 photos added to gallery of products

--- client/src/pages/Doors.jsx
+++ client/src/pages/Doors.jsx
@@ -8,7 +8,7 @@ function Doors(){
         <div className="mt-28 grid md:mt36 mb-12">
             <div className="bg-header mb-12 h-auto text-white py-10 md:py-24 md:px-10 object-fill">
                 <h2 className="text-center text-3xl mb-8 font-bold md:text-4xl md:mb-16">Kapılar</h2>
-                <ul className="flex px-4 items-center justify-evenly text-center md:text-xl md:px-8">
+                <ul className="flex px-4 items-center justify-evenly text-center md:text-2xl md:px-8">
                     <li>Daire Kapıları</li>
                     <DotIcon className="w-2"/>
                     <li>Villa Kapıları</li>
@@ -28,6 +28,9 @@ function Doors(){
                         </div>          
                     )
                 })}
+            </div>
+            <div className="text-center text-xl my-8 px-4">
+                <p>Daha fazla ürün ve renk seçenekleri için bizimle <a className="font-bold underline" href="/iletisim">iletişime geçin.</a></p>
             </div>
             <h3 className="text-header p-6 font-bold text-xl md:text-6xl">Markalar</h3>
         </div>

1c0e43b72ed86cfeb8455c70507b6aaaad47f4c6
Armagan Dalkiran <armagandalkran20@gmail.com>
2021-08-17 00:51:53 +0300

    🐱‍👤 last updates

--- client/src/pages/References.jsx
+++ client/src/pages/References.jsx
@@ -4,8 +4,8 @@ export default function References() {
     return (
         <div className="mt-28 md:mt-36">
             <div>
-                <h3 className="text-header p-6 font-bold text-2xl md:text-6xl">Marmaris Referanslarımız</h3>
-                <div className="p-6 flex flex-col gap-y-4 items-center justify-center md:grid md:grid-cols-4 md:gap-x-4">
+                <h3 className="text-header underline p-6 my-8 font-bold text-2xl md:text-4xl">Marmaris Referanslarımız</h3>
+                <div className="p-6 flex flex-col gap-y-12  items-center justify-center md:grid md:grid-cols-4 md:gap-x-4">
                     <div className="flex items-center justify-center">
                         <img className="p-4" src="/assets/marmarisyapi-logo.svg" alt="marmaris-yapi-logo"/>
                     </div>
@@ -13,13 +13,7 @@ export default function References() {
                         <img className="p-4" src="/assets/artmaris-logo.svg" alt="artmaris-logo"/>
                     </div>
                     <div className="flex items-center justify-center">
-                        <img className="p-4" src="/assets/vizyon-logo.svg" alt="vizyon-logo"/>
-                    </div>
-                    <div className="flex items-center justify-center">
                         <img className="p-4" src="/assets/yigit-yapi.svg" alt="yigit-yapi-logo"/>
-                    </div>
-                    <div className="flex items-center justify-center">
-                        <img className="p-4" src="/assets/goksan.svg" alt="goksan-logo"/>
                     </div>
                     <div className="flex items-center justify-center">
                         <img className="p-4" src="/assets/cmt-yapi.svg" alt="cmt-yapi-logo"/>
@@ -45,12 +39,46 @@ export default function References() {
                 </div>
             </div>    
             <div>
-                <h3 className="text-header p-6 font-bold text-2xl md:text-6xl">Datça Referanslarımız</h3>
-                
+                <h3 className="text-header underline p-6 my-8 font-bold text-2xl md:text-4xl">Datça Referanslarımız</h3>
+                <div className="p-6 flex flex-col gap-y-12  items-center justify-center md:grid md:grid-cols-4 md:gap-x-4">
+                    <div className="flex items-center justify-center">
+                        <img className="p-4" src="/assets/againsaat-datca.png" alt="againsaat-logo"/>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <img className="p-4" src="/assets/kesa-insaat.png" alt="kesainsaat-logo"/>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <img className="p-4" src="/assets/vizyon-logo.svg" alt="vizyon-logo"/>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <img className="p-4" src="/assets/lindos.png" alt="lindos-logo"/>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <img className="p-4" src="/assets/karia.png" alt="karia-logo"/>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <img className="p-4" src="/assets/ihsanfatih.png" alt="ihsanfatih-logo"/>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <img className="p-4" src="/assets/baranyapi.png" alt="baranyapi-logo"/>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <img className="p-4" src="/assets/lacininsaat.png" alt="lacininsaat-logo"/>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <img className="p-4" src="/assets/goksan.svg" alt="goksan-logo"/>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <img className="p-4" src="/assets/vehbitemelinsaat.svg" alt="vehbi-logo"/>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <img className="p-4" src="/assets/kormeninsaat.png" alt="kormen-logo"/>
+                    </div>
+                </div>
             </div>
             <div>
-                <h3 className="text-header p-6 font-bold text-2xl md:text-6xl">Otel Referanslarımız</h3>
-                <div className="p-6 flex flex-col gap-y-4 items-center justify-center md:grid md:grid-cols-4 md:gap-x-4">
+                <h3 className="text-header p-6 underline my-8 font-bold text-2xl md:text-4xl">Otel Referanslarımız</h3>
+                <div className="p-6 mb-20 flex flex-col gap-y-12 items-center justify-center md:grid md:grid-cols-4 md:gap-x-4">
                     <div className="flex items-center justify-center">
                         <img className="p-4" src="/assets/idealhotels.jpg" alt="idealhotels-logo"/>
                     </div>
@@ -69,6 +97,27 @@ export default function References() {
                     </div>
                     <div className="flex items-center justify-center">
                         <img className="p-4 w-48" src="/assets/grand-cettia-logo.png" alt="cettia-logo"/>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <img className="p-4" src="/assets/capekrio.png" alt="capekrio-logo"/>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <img className="p-4" src="/assets/yalcinkaya.svg" alt="yalcinkaya-logo"/>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <img className="p-4" src="/assets/hotelmare.svg" alt="hotelmare-logo"/>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <img className="p-4" src="/assets/idashotel.png" alt="idas-logo"/>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <img className="p-4" src="/assets/letoile.png" alt="letoile-logo"/>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <img className="p-4" src="/assets/fortune.png" alt="letoile-logo"/>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <img className="p-4" src="/assets/orca.png" alt="orca-logo"/>
                     </div>
                 </div>
             </div>

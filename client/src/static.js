f0a4156f897fc9d5859805971e49becd6a8fdac4
Armagan Dalkiran <armagandalkran20@gmail.com>
2021-08-16 13:39:40 +0300

    ✔ public assets added !

--- client/src/static.js
+++ client/src/static.js
@@ -2,227 +2,228 @@ const images = {
     kapi:[
         {
             desc:"STEP TEAK - Ahşap Kaplama Serisi",
-            url: "https://res.cloudinary.com/yorusoft-com/image/upload/v1628857131/eliteparke/stepteakrm03_epe4ze.jpg",
+            url: "/assets/stepteakrm03_epe4ze.jpg",
         },
         {
             desc:"SL 03 (Siyah/Beyaz) - Lake Serisi",
-            url: "https://res.cloudinary.com/yorusoft-com/image/upload/v1628857243/eliteparke/sl03_t5ougf.jpg",
+            url: "/assets/sl03_t5ougf.jpg",
         },
         {
             desc:"ADONIS - Membran Serisi",
-            url: "https://res.cloudinary.com/yorusoft-com/image/upload/v1628857360/eliteparke/adonime20_m0jiz4.jpg",
+            url: "/assets/adonime20_m0jiz4.jpg",
         },
         {
             desc:"PRJ-14 - Woodlam Serisi",
-            url: "https://www.surcelikkapi.com/dosya/proj14.jpg",
+            url: "/assets/proj14.jpg",
         },
         {
             desc:"Selen F30 Likya Kartlı Kilitli Yangına Dayanıklı Ahşap Kapı",
-            url: "https://st2.myideasoft.com/idea/ee/92/myassets/products/668/9_min.jpg?revision=1622551684",
+            url: "/assets/9_min.jpg",
         },
         {
             desc:"Indoor Life F30 Yangına Dayanıklı Ahşap Otel Kapısı",
-            url: "https://st1.myideasoft.com/idea/ee/92/myassets/products/570/f30yanginkapisi.jpg?revision=1616747259",
+            url: "/assets/f30yanginkapisi.jpg",
         },
         {
             desc:"Mercan F90 Likya Yangına Dayanıklı Ahşap Kapı",
-            url: "https://st1.myideasoft.com/idea/ee/92/myassets/products/524/90dk-yangina-dayanikli-kapi.jpg?revision=1614257280",
+            url: "/assets/90dk-yangina-dayanikli-kapi.jpg",
         },
         {
             desc:"Mercan Indoor Life F30G Doğal Ceviz Yangına Dayanıklı Ahşap Otel Kapısı",
-            url: "https://st3.myideasoft.com/idea/ee/92/myassets/products/030/11.jpg?revision=1613029927",
+            url: "/assets/11.jpg",
         },
         {
             desc:"- LAKE KAPILAR -",
-            url: "https://westdoor.com.tr/images/2020kapi/6.jpg",
+            url: "/assets/6.jpg",
         },
         {
             desc:"- PVC KAPILAR -",
-            url: "https://westdoor.com.tr/images/2020kapi/1.jpg",
+            url: "/assets/1.jpg",
         },
         {
             desc:"- Doğal Ahşap Kapılar -",
-            url: "https://westdoor.com.tr/images/2020kapi/8.jpg",
+            url: "/assets/8.jpg",
         },
         {
             desc:"- Naturlam Kapılar -",
-            url: "https://westdoor.com.tr/images/2020kapi/3.jpg",
+            url: "/assets/3.jpg",
         },
         {
             desc:"- Yangın Kapısı -",
-            url: "https://www.surcelikkapi.com/dosya/yangin1.jpg",
+            url: "/assets/yangin1.jpg",
         },
         
     ],
-    lamine:[        {
-        desc:"SERIFOGLU - Sapelli / Maksi Plank",
-        url: "http://www.serifoglu.com.tr/UserFiles/SayfaSonsuz/big/772164334.jpg",
-    },
-    {
-        desc:"SERIFOGLU - Meşe / Fırçalı Yağlı / Maksi Plank",
-        url: "http://www.serifoglu.com.tr/UserFiles/SayfaSonsuz/big/399915109.jpg",
-    },
-    {
-        desc:"SERIFOGLU - Meşe / Dalgalı / Maksi Plank",
-        url: "http://www.serifoglu.com.tr/UserFiles/SayfaSonsuz/big/203734141.jpg",
-    },
-    {
-        desc:"SERIFOGLU - Burma Teak / Plank",
-        url: "http://www.serifoglu.com.tr/UserFiles/SayfaSonsuz/big/193311886.jpg",
-    },
-    {
-        desc:"SERIFOGLU - Meşe R-56 / Plank",
-        url: "http://www.serifoglu.com.tr/UserFiles/SayfaSonsuz/big/190558906.jpg",
-    },
-    {
-        desc:"SERIFOGLU - Meşe R-53 / Maksi Plank",
-        url: "http://www.serifoglu.com.tr/UserFiles/SayfaSonsuz/big/660489567.jpg",
-    },
-    {
-        desc:"SERIFOGLU - Meşe Rustik / Fırçalı Yağlı / XL Plank",
-        url: "http://www.serifoglu.com.tr/UserFiles/SayfaSonsuz/big/170503029.jpg",
-    },
-            
-    ],
-    laminant:[
+    lamine:[        
         {
-            desc:"AGT - Bella Serisi",
-            url: "https://www.agt.com.tr/i/assets/images/content/bella-promo-2017.jpg",
+            desc:"SERIFOGLU - Sapelli / Maksi Plank",
+            url: "/assets/772164334.jpg",
         },
         {
-            desc:"AGT - Natura Select Serisi",
-            url: "https://www.agt.com.tr/i/assets/images/content/natura-parke-product-2.jpg",
+            desc:"SERIFOGLU - Meşe / Fırçalı Yağlı / Maksi Plank",
+            url: "/assets/399915109.jpg",
         },
         {
-            desc:"AGT - Pruva Parke Serisi",
-            url: "https://www.agt.com.tr/i/assets/images/content/pruva-parke-product.jpg",
+            desc:"SERIFOGLU - Meşe / Dalgalı / Maksi Plank",
+            url: "/assets/203734141.jpg",
         },
         {
-            desc:"AGT - Natura Line Serisi",
-            url: "https://www.agt.com.tr/i/assets/images/content/natura-line-product-2.jpg",
+            desc:"SERIFOGLU - Burma Teak / Plank",
+            url: "/assets/193311886.jpg",
         },
         {
-            desc:"AGT - Effect Serisi",
-            url: "https://www.agt.com.tr/i/assets/images/content/effect_detay.jpg",
+            desc:"SERIFOGLU - Meşe R-56 / Plank",
+            url: "/assets/190558906.jpg",
         },
         {
-            desc:"AGT - Armonia Large Serisi",
-            url: "https://www.agt.com.tr/i/assets/images/content/natura-large-promo-2017.jpg",
+            desc:"SERIFOGLU - Meşe R-53 / Maksi Plank",
+            url: "/assets/660489567.jpg",
         },
         {
-            desc:"AGT - Armonia Slim Serisi",
-            url: "https://www.agt.com.tr/i/assets/images/content/natura-slim-parke-2017.jpg",
+            desc:"SERIFOGLU - Meşe Rustik / Fırçalı Yağlı / XL Plank",
+            url: "/assets/170503029.jpg",
         },
-        {
-            desc:"AGT - Concept Neo Serisi",
-            url: "https://www.agt.com.tr/i/assets/images/content/concept_scala_detay.jpg",
-        },
-        {
-            desc:"FLOORPAN CLASSIC - GÖKOVA",
-            url: "https://www.floorpan.com.tr/images/urunleryeni/classic/21.jpg",
-        },
-        {
-            desc:"FLOORPAN CLASSIC - GENOVA",
-            url: "https://www.floorpan.com.tr/images/urunleryeni/classic/FC081%20GENOVA.jpg",
-        },
-        {
-            desc:"FLOORPAN ELITE - EMİRGAN",
-            url: "https://www.floorpan.com.tr/Resimler/f67117cb-7171-4683-8362-810a2ee52110_o-f62b8334-554f-42a0-b1df-509b699631.jpg",
-        },
-        {
-            desc:"FLOORPAN FIX - BORA",
-            url: "https://www.floorpan.com.tr/images/urunleryeni/fix/FF003%20Bora.jpg",
-        },
-        {
-            desc:"FLOORPAN NATURAL - ANTİK BEYAZ",
-            url: "https://www.floorpan.com.tr/images/urunleryeni/natural/8.jpg",
-        },
-        {
-            desc:"FLOORPAN NOVA - MANOLYA",
-            url: "https://www.floorpan.com.tr/images/urunleryeni/nova/4.jpg",
-        },
-        {
-            desc:"FLOORPAN REGISTER - NİL",
-            url: "https://www.floorpan.com.tr/images/urunleryeni/register/3.jpg",
-        },
+                
+        ],
+        laminant:[
+            {
+                desc:"AGT - Bella Serisi",
+                url: "/assets/bella-promo-2017.jpg",
+            },
+            {
+                desc:"AGT - Natura Select Serisi",
+                url: "/assets/natura-parke-product-2.jpg",
+            },
+            {
+                desc:"AGT - Pruva Parke Serisi",
+                url: "/assets/pruva-parke-product.jpg",
+            },
+            {
+                desc:"AGT - Natura Line Serisi",
+                url: "/assets/natura-line-product-2.jpg",
+            },
+            {
+                desc:"AGT - Effect Serisi",
+                url: "/assets/effect_detay.jpg",
+            },
+            {
+                desc:"AGT - Armonia Large Serisi",
+                url: "/assets/natura-large-promo-2017.jpg",
+            },
+            {
+                desc:"AGT - Armonia Slim Serisi",
+                url: "/assets/natura-slim-parke-2017.jpg",
+            },
+            {
+                desc:"AGT - Concept Neo Serisi",
+                url: "/assets/concept_scala_detay.jpg",
+            },
+            {
+                desc:"FLOORPAN CLASSIC - GÖKOVA",
+                url: "/assets/21.jpg",
+            },
+            {
+                desc:"FLOORPAN CLASSIC - GENOVA",
+                url: "/assets/FC081%20GENOVA.jpg",
+            },
+            {
+                desc:"FLOORPAN ELITE - EMİRGAN",
+                url: "/assets/f67117cb-7171-4683-8362-810a2ee52110_o-f62b8334-554f-42a0-b1df-509b699631.jpg",
+            },
+            {
+                desc:"FLOORPAN FIX - BORA",
+                url: "/assets/FF003%20Bora.jpg",
+            },
+            {
+                desc:"FLOORPAN NATURAL - ANTİK BEYAZ",
+                url: "/assets/88.jpg",
+            },
+            {
+                desc:"FLOORPAN NOVA - MANOLYA",
+                url: "/assets/44.jpg",
+            },
+            {
+                desc:"FLOORPAN REGISTER - NİL",
+                url: "/assets/33.jpg",
+            },
         
     ],
     supurgelik:[
         {
             desc:"FLOORPAN - AKDENİZ ÇAM",
-            url: "https://www.floorpan.com.tr/images/supurgelikyeni/1.jpg",
+            url: "/assets/111.jpg",
         },
         {
             desc:"FLOORPAN - ANTİK BEYAZ",
-            url: "https://www.floorpan.com.tr/images/supurgelikyeni/2.jpg",
+            url: "/assets/222.jpg",
         },
         {
             desc:"FLOORPAN - ASOS MEŞE",
-            url: "https://www.floorpan.com.tr/images/supurgelikyeni/3.jpg",
+            url: "/assets/333.jpg",
         },
         {
             desc:"FLOORPAN - BEYAZ",
-            url: "https://www.floorpan.com.tr/images/supurgelikyeni/5.jpg",
+            url: "/assets/55.jpg",
         },
         {
             desc:"FLOORPAN - DOUSSIE",
-            url: "https://www.floorpan.com.tr/images/supurgelikyeni/8.jpg",
+            url: "/assets/888.jpg",
         },
         {
             desc:"FLOORPAN - KARADENİZ MEŞE",
-            url: "https://www.floorpan.com.tr/images/supurgelikyeni/13.jpg",
+            url: "/assets/13.jpg",
         },
         {
             desc:"FLOORPAN - KÜL MEŞE",
-            url: "https://www.floorpan.com.tr/images/supurgelikyeni/14.jpg",
+            url: "/assets/14.jpg",
         },
         {
             desc:"FLOORPAN - TOROS ÇAM",
-            url: "https://www.floorpan.com.tr/images/supurgelikyeni/18.jpg",
+            url: "/assets/18.jpg",
         },
         {
             desc:"FLOORPAN - ULUDAG MESE",
-            url: "https://www.floorpan.com.tr/images/supurgelikyeni/21.jpg",
+            url: "/assets/211.jpg",
         },
         {
             desc:"VEVO - FOLYO LAKE ASOS BEYAZ",
-            url: "https://www.vevodizayn.com.tr/images/urunler/202099f978cde6-c7d8-429b-9b19-ae9ed2fbf17d.png",
+            url: "/assets/202099f978cde6-c7d8-429b-9b19-ae9ed2fbf17d.png",
         },
         {
             desc:"VEVO - FOLYO LAKE BODRUM BEYAZ",
-            url: "https://www.vevodizayn.com.tr/images/urunler/2020998aea3059-54bd-4635-b25f-fc7f65bdccb5.png",
+            url: "/assets/2020998aea3059-54bd-4635-b25f-fc7f65bdccb5.png",
         },
         {
             desc:"VEVO - FOLYO LAKE ÇEŞME BEYAZ",
-            url: "https://www.vevodizayn.com.tr/images/urunler/2020993021bca0-5f0b-43b7-a33b-a1903e57ed52.png",
+            url: "/assets/2020993021bca0-5f0b-43b7-a33b-a1903e57ed52.png",
         },
         {
             desc:"VEVO - SOFT TOUCH BURÇ HALİF BEYAZ",
-            url: "https://www.vevodizayn.com.tr/images/urunler/2020910abb0860f-c4cd-4bf8-8aa5-179de4f49d9b.png",
+            url: "/assets/2020910abb0860f-c4cd-4bf8-8aa5-179de4f49d9b.png",
         },
         {
             desc:"VEVO - SOFT TOUCH GT FIRTINA GRI",
-            url: "https://www.vevodizayn.com.tr/images/urunler/20201026b6e5977d-0135-4591-8843-10f4773acf4e.png",
+            url: "/assets/20201026b6e5977d-0135-4591-8843-10f4773acf4e.png",
         },
         {
             desc:"VEVO - SOFT TOUCH KEMER BEYAZ",
-            url: "https://www.vevodizayn.com.tr/images/urunler/202099b75026a4-b518-4208-ae15-1fde4a1f3315.png",
+            url: "/assets/202099b75026a4-b518-4208-ae15-1fde4a1f3315.png",
         },
         {
             desc:"VEVO - PVC LAKE GALATA BEYAZ",
-            url: "https://www.vevodizayn.com.tr/images/urunler/202091096c60ce6-766a-4797-a009-e955f3fe5e28.png",
+            url: "/assets/202091096c60ce6-766a-4797-a009-e955f3fe5e28.png",
         },
         {
             desc:"VEVO - HIGH GLOSS SU 5040 - YENİ SİYAH",
-            url: "https://www.vevodizayn.com.tr/images/urunler/2020910c2d87cb0-ccc6-44e9-9156-d6fe9f797890.png",
+            url: "/assets/2020910c2d87cb0-ccc6-44e9-9156-d6fe9f797890.png",
         },
         {
             desc:"VEVO - PICASSO KULE GOLD",
-            url: "https://www.vevodizayn.com.tr/images/urunler/20201026bfdfb2ff-b557-448c-8454-328d97299114.png",
+            url: "/assets/20201026bfdfb2ff-b557-448c-8454-328d97299114.png",
         },
         {
             desc:"VEVO - SATEN TOPKAPI-ANTRASIT",
-            url: "https://www.vevodizayn.com.tr/images/urunler/202010267c6042c1-2eb5-41b3-8e7a-f956f11634c9.png",
+            url: "/assets/202010267c6042c1-2eb5-41b3-8e7a-f956f11634c9.png",
         },
     ],
     markalar:[
